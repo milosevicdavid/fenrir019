f4e91556471fe737108abb5ba990f351eff786ce
David <milosevicdavid@gmail.com>
2020-09-16 22:04:22 +0200

    start

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,11 +24,11 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        start_url: `/Contact.js/`,
+        start_url: `/index`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
