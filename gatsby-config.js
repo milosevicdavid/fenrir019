f0d07125e7b37a87e9121be96c312f9f4deafbae
David <milosevicdavid@gmail.com>
2020-09-16 21:56:38 +0200

    Contact js

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        start_url: `/index.js/`,
+        start_url: `/Contact.js/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
