09006585ef992907167d63032aa04cd617b6b0a5
David <milosevicdavid@gmail.com>
2020-09-16 21:50:15 +0200

    starting page

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        start_url: `/`,
+        start_url: `/index.js/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
