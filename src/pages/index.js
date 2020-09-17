13d946154ac8e0b0233d985cdd61737f3a31f61d
David <milosevicdavid@gmail.com>
2020-09-17 08:25:33 +0200

    Added grid on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Layout from "../components/layout"
-import {  graphql } from "gatsby"
+import { graphql } from "gatsby"
 import Img from "gatsby-image"
 import { FaInstagram } from "react-icons/fa"
 import { GiHouse, GiHealthNormal, GiPawHeart } from "react-icons/gi"
@@ -10,88 +10,95 @@ import "../components/layout.css"
 const IndexPage = ({ data }) => {
   return (
     <main>
-      
       <Layout>
-        <div className="text">
-          <p className="headline">
-            U VAŠEM KOMŠILUKU
-            <span className="icon">
-              <GiHouse />
-            </span></p>
+        <div className="index-container">
+          <div className="grid-cell">
+            <div className="text">
+              <p className="headline">
+                TIM VETERINARA
+                <span className="icon">
+                  <GiHealthNormal />
+                </span>
+              </p>
 
-          <p>
-            Fenrir019 veterinarska ambulanta je specijalizovana za pružanje
-            preventive i terapije Vašim ljubimcima. Nalazimo se na teritoriji
-            opštine Palilula, u ulici Marijane Gregoran 34a.
-          </p>
-        </div>
-        <div className="cover">
-          <Img className="cover" fluid={data.first.childImageSharp.fluid} />
-        </div>
+              <p>
+                U našoj ambulanti rade dva licencirana doktora veterinarske
+                medicine, koji će svojom stručnošću postaviti pravu dijagnozu i
+                dati terapiju, po potrebi. Ljubav prema životinjama je glavni
+                razlog zašto naša veterinarska ambulanta FENRIR019 postoji.
+              </p>
+            </div>
+            <div className="cover">
+              <Img
+                className="cover"
+                fluid={data.second.childImageSharp.fluid}
+              />
+            </div>
+          </div>
+          <div className="grid-cell">
+            <div className="text">
+              <p className="headline">
+                U VAŠEM KOMŠILUKU
+                <span className="icon">
+                  <GiHouse />
+                </span>
+              </p>
 
-        <div className="text">
-          <p className="headline">
-            TIM VETERINARA
-            <span className="icon">
-              <GiHealthNormal />
-            </span>
-          </p>
+              <p>
+                Fenrir019 veterinarska ambulanta je specijalizovana za pružanje
+                preventive i terapije Vašim ljubimcima. Nalazimo se na
+                teritoriji opštine Palilula, u ulici Marijane Gregoran 34a.
+              </p>
+            </div>
+            <div className="cover">
+              <Img className="cover" fluid={data.first.childImageSharp.fluid} />
+            </div>
+          </div>
+          <div className="grid-cell">
+            <div className="text">
+              <p className="headline">
+                LJUBIMCI
+                <span className="icon">
+                  <GiPawHeart />
+                </span>
+              </p>
 
-          <p>
-            U našoj ambulanti rade dva licencirana doktora veterinarske
-            medicine, koji će svojom stručnošću postaviti pravu dijagnozu i dati
-            terapiju, po potrebi. Ljubav prema životinjama je glavni razlog
-            zašto naša veterinarska ambulanta FENRIR019 postoji.
-          </p>
-        </div>
-        <div className="cover">
-          <Img className="cover" fluid={data.second.childImageSharp.fluid} />
-        </div>
+              <p>
+                Veterinarska ambulanta FENRIR019 pre svega sve svoje drage
+                pacijente tretira jednako s ljubavlju i pažnjom a iznad svega
+                nam je stalo da na najbolji i najadekvatniji način nađemo
+                rešenje za problem koji ih muči bilo da su u oklopu, bodljikavi,
+                pernati, čupavi, nervozni, uplašeni, ljuti...
+              </p>
+            </div>
+            <div className="cover-container">
+              <Img className="cover" fluid={data.third.childImageSharp.fluid} />
+            </div>
+          </div>
 
-        <div className="text">
-          <p className="headline">
-            LJUBIMCI
-            <span className="icon">
-              <GiPawHeart />
-            </span>
-          </p>
+          <div className="grid-cell">
+            <div className="text">
+              <p className="headline">
+                NA INSTAGRAMU
+                <span className="icon">
+                  <FaInstagram />
+                </span>
+              </p>
 
-          <p>
-            Veterinarska ambulanta FENRIR019 pre svega sve svoje drage pacijente
-            tretira jednako s ljubavlju i pažnjom a iznad svega nam je stalo da
-            na najbolji i najadekvatniji način nađemo rešenje za problem koji ih
-            muči bilo da su u oklopu, bodljikavi, pernati, čupavi, nervozni,
-            uplašeni, ljuti...
-          </p>
-        </div>
-        <div className="cover-container">
-          <Img className="cover" fluid={data.third.childImageSharp.fluid} />
-        </div>
-
-        <div className="text">
-          <p className="headline">
-            U VAŠEM KOMŠILUKU
-            <span className="icon">
-              <FaInstagram/>
-            </span>
-          </p>
-
-          <p> 
-            Sprovodimo akcije za određene usluge na nedeljnom nivou. Pratite rad
-            naše ambulante na Instagramu: @fenrir019veterinar.
-          </p>
-        </div>
-        <div className="cover">
-          <Img className="cover" fluid={data.first.childImageSharp.fluid} />
-        </div>
+              <p>
+                Sprovodimo akcije za određene usluge na nedeljnom nivou. Pratite
+                rad naše ambulante na Instagramu: @fenrir019veterinar.
+              </p>
+            </div>
+            <div className="cover">
+              <Img className="cover" fluid={data.first.childImageSharp.fluid} />
+            </div>
+          </div>
+          </div>
       </Layout>
-      
     </main>
   )
 }
-
-
-
 
 export const query = graphql`
   query {
